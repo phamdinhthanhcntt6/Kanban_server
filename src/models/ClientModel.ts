efefed064bfb14bc35e4a71a4a4f64470b71b623
phamdinhthanhcntt6 <90460582+phamdinhthanhcntt6@users.noreply.github.com>
2025-02-24 13:47:37 +0700

    client API

---
+++ src/models/ClientModel.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+const ClientSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  photoUrl: String,
+  rule: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const ClientModel = mongoose.model("clients", ClientSchema);
+export default ClientModel;
---
+++ src/routers/client.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {
+  login,
+  loginWithGoogle,
+  refreshToken,
+  register,
+} from "../controllers/client";
+
+const router = Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/google-login", loginWithGoogle);
+router.get("/refresh-token", refreshToken);
+
+export default router;
