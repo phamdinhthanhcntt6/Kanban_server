c7dec8048eaccfe7ed9d7a1e3332ce7aaecbde7f
phamdinhthanhcntt6 <90460582+phamdinhthanhcntt6@users.noreply.github.com>
2025-02-26 23:58:38 +0700

    update

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ import storageRouter from "./src/routers/storage";
 import supplierRouter from "./src/routers/supplier";
 import userRouter from "./src/routers/user";
 import subProductRouter from "./src/routers/subProduct";
-import clientRouter from "./src/routers/client";
+import customerRouter from "./src/routers/client";
 
 dotenv.config();
 
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cors({}));
 
 app.use("/auth", userRouter);
-app.use("/client", clientRouter);
+app.use("/customer", customerRouter);
 
 app.use(verifyToken);
 app.use("/storage", storageRouter);
